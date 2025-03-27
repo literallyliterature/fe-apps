a79ceb7560c4460a9a66144bc2b6f7a4fdcd3346
Sahitya Beri <sahityaberi@gmail.com>
2025-03-27 14:22:42 +1100

    Initialise apps-host app
    with Vue and Vuetify installed

---
+++ apps/apps-host/src/plugins/vuetify.ts
@@ -0,0 +1,25 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: { fa },
+  },
+  theme: {
+    defaultTheme: 'light',
+  },
+})
