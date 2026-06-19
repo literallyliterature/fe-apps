2723816e64820637070c13c36494aa140761c0af
Sahitya Beri <sahityaberi@gmail.com>
2026-06-20 08:44:41 +1000

    Install eslint perfectionist plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import antfu from '@antfu/eslint-config';
+import perfectionist from 'eslint-plugin-perfectionist';
 
-export default antfu({
-  rules: {
-    'antfu/if-newline': ['off'],
-    'no-alert': ['off'],
-    'style/semi': ['error', 'always'],
+export default antfu(
+  {
+    rules: perfectionist.configs['recommended-natural'].rules,
   },
-});
+  {
+    rules: {
+      'antfu/if-newline': ['off'],
+      'no-alert': ['off'],
+      'style/semi': ['error', 'always'],
+    },
+  },
+);
